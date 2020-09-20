da88ce0103845bc45e33ddb9370cc43935604806
Никита Морозов <nikita@MacBook-Pro-Nikita.local>
2020-09-20 19:50:57 +0300

    formatted code

--- back.py
+++ back.py
@@ -7,67 +7,74 @@ import requests
 app = FastAPI(title="Landmark Operations")
 DB_NAME = 'marks.db'
 
-@app.get("/get"
-        )
-def get_all_landmarks(response:Response ): #gets all landmarks from db and sends them
-        conn = sqlite3.connect(DB_NAME)
-        cursor = conn.cursor()
-        cursor.execute("""
 
-        --sql 
-        SELECT * FROM marks WHERE status = 1 
+@app.get("/get"
+         )
+# gets all landmarks from db and sends them
+def get_all_landmarks(response: Response):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""
+
+        --sql
+        SELECT * FROM marks WHERE status = 1
         ;
 
         """)
-        landmarks_dict = {i+1: {'longitude': rec[2], 'latitude': rec[1]} for i, rec in enumerate(cursor)}
-        conn.commit()
-        conn.close()
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        return {'detail': landmarks_dict
-                }
+    landmarks_dict = {
+        i + 1: {'longitude': rec[2], 'latitude': rec[1]} for i, rec in enumerate(cursor)}
+    conn.commit()
+    conn.close()
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return {'detail': landmarks_dict}
+
 
 @app.get("/add"
-        )
-def add_landmark(long: float, lat: float): #adds landmark to database with latitude and longitude given
-        conn = sqlite3.connect(DB_NAME)
-        cursor = conn.cursor()
-        cursor.execute("""
+         )
+# adds landmark to database with latitude and longitude given
+def add_landmark(long: float, lat: float):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""
             --sql
-            INSERT INTO marks (lat, long, mark_date, status) 
+            INSERT INTO marks (lat, long, mark_date, status)
             VALUES(?,?,?,1)
             ;
-        """,(lat, long, datetime.now().strftime('%c'),)
-        )
-        conn.commit()
-        conn.close()
-        return 
+        """, (lat, long, datetime.now().strftime('%c'),)
+    )
+    conn.commit()
+    conn.close()
+    return
+
 
 @app.get("/delete"
-        )
-def add_landmark(long: float, lat: float): #deletes landmark to database with latitude and longitude given
-        conn = sqlite3.connect(DB_NAME)
-        cursor = conn.cursor()
-        cursor.execute("""
+         )
+# deletes landmark to database with latitude and longitude given
+def add_landmark(long: float, lat: float):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""
             --sql
-            UPDATE marks SET status = 0 
+            UPDATE marks SET status = 0
                 WHERE lat = ? AND long = ?
             ;
-        """,(lat, long, )
-        )
-        conn.commit()
-        conn.close()
-        return         
+        """, (lat, long, )
+    )
+    conn.commit()
+    conn.close()
+    return
+
 
 def main():
-    def db_worker(): #creates database and puts random mark there
+    def db_worker():  # creates database and puts random mark there
         conn = sqlite3.connect(DB_NAME)
         cursor = conn.cursor()
         cursor.executescript("""
-        --sql 
+        --sql
         PRAGMA foreign_keys = on
         ;
 
-        --sql 
+        --sql
         CREATE TABLE IF NOT EXISTS marks
         (mark_id INTEGER PRIMARY KEY AUTOINCREMENT,
         lat INTEGER,
@@ -77,18 +84,18 @@ def main():
         )
         ;
         """
-        )
+                             )
         cursor.execute("""
             --sql
-            INSERT INTO marks (lat, long, mark_date, status) 
+            INSERT INTO marks (lat, long, mark_date, status)
             VALUES(?,?,?,1)
             ;
-        """,(random.uniform(-90,90), random.uniform(-90,90), datetime.now().strftime('%c'),)
+        """, (random.uniform(-90, 90), random.uniform(-90, 90), datetime.now().strftime('%c'),)
         )
         conn.commit()
         conn.close()
     db_worker()
 
+
 if __name__ == '__main__':
     main()
-    
